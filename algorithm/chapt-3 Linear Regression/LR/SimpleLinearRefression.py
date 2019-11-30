c6774ff616db65f82577b90fa6a313736463c9af
jsfantasy <1377378268@qq.com>
2019-11-30 19:02:31 +0100

    add MAE and RMSE

--- algorithm/chapt-3 Linear Regression/LR/SimpleLinearRefression.py
+++ algorithm/chapt-3 Linear Regression/LR/SimpleLinearRefression.py
@@ -42,3 +42,44 @@ class SimpleLinearRegression1:
 
 	def __repr__(self):
 		return "SimpleLinearRegression1()"
+
+# 使用向量化实现
+class SimpleLinearRegression:
+
+	def __init__(self):
+		# 初始化 SimpleLinearRegression 模型
+		self.a_ = None
+		self.b_ = None
+
+	def fit(self, x_train, y_train):
+		# 根据训练数据集训练模型
+		assert x_train.ndim == 1, "Simple Linear Regression can only solve single feature train data"
+		assert len(x_train) == len(y_train), \
+			"The size of x_train must be equal to the size of y_train"
+
+		x_mean = np.mean(x_train)
+		y_mean = np.mean(y_train)
+
+		num = (x_train - x_mean).dot(y_train - y_mean)
+		d = (x_train - x_mean).dot(x_train - x_mean)
+
+		self.a_ = num / d
+		self.b_ = y_mean - self.a_ * x_mean
+
+		return self
+
+	def predict(self, x_predict):
+		# 给定待预测数据集 x_predict ,返回预测的结果向量
+		assert x_predict.ndim == 1, \
+			"Simple Linear Regression can only solve single feature train data"
+		assert self.a_ is not None and self.b_ is not None, \
+			"must fit before predict"
+
+		return np.array([self._predict(x) for x in x_predict])
+
+	def _predict(self, x_single):
+		# 给定单个待预测数据 x_single, 返回x_single 的预测结果值
+		return self.a_ * x_single + self.b_
+
+	def __repr__(self):
+		return "SimpleLinearRegression()"
